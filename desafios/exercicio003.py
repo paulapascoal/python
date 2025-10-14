9cf646ec8cea7580fda66ec8d46761bfa27bc44f
Paula Pascoal <paulaiasmimpascoal2012@gmail.com>
2025-10-14 15:20:39 -0300

    desafios 003 e 004

---
+++ desafios/exercicio003.py
@@ -0,0 +1,5 @@
+print("Vamos fazer uma continha simples de Matemática :)");
+num1 = int(input("Digite o primeiro número: "));
+num2 = int(input("Digite o segundo número: "));
+soma = num1 + num2;
+print(f"A soma entre {num1} e {num2} é {soma}!");
---
+++ desafios/exercicio004.py
@@ -0,0 +1,8 @@
+a = input("Escreva algo: ");
+print ("O tipo primitivo desse valor é: ",type(a));
+print ("Só tem espaços? ", a.isspace());
+print ("É um número? ", a.isnumeric());
+print ("É alfabético?", a.isalpha());
+print ("Está em maiúscula? ", a.isupper());
+print ("Está em minúscula?", a.islower());
+print ("Está capitalizada?", a.istitle());
